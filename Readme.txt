75dc0aae8abc6bc1dcfc49f71a37c5abb2ac65e4
Chaseis4344 <73896908+Chaseis4344@users.noreply.github.com>
2021-11-18 10:25:45 -0500

    First commit

---
+++ Readme.txt
@@ -0,0 +1,18 @@
+
+  Context: Canvas has a Plugin called A+ Attendance, which my school uses for Attendance purposes
+  I enjoy breaking things, so what better way to break attendance than to stroe every possible code
+  it will ever generate on a single file?
+
+  Make something that does it automatically
+
+  WARNING DO NOT RUN THIS ON A WEB BASED COMPILER
+  IT WILL CRASH YOUR COMPILER OF CHOICE
+
+  Running on physical hardware it takes roughly 10 minutes to run, it's not a fast algorithm
+  The last number should be ZZZZZ.
+
+  I may update this project in future to feature more advanced swap algorithms or easier/faster base conversion, 
+  another second bottleneck is writing to file
+
+  The exact number of permutations included in the text file is 58786561
+  The file this Generates gives roughly 355,000 KB of pure text data, if you
---
+++ main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include "string.h"
+#include <fstream>
+
+/*
+  Context: Canvas has a Plugin called A+ Attendance, which my school uses for Attendance purposes
+  I enjoy breaking things, so what better way to break attendance than to stroe every possible code
+  it will ever generate on a single file?
+
+  Make something that does it automatically
+
+  WARNING DO NOT RUN THIS ON A WEB BASED COMPILER
+  IT WILL CRASH YOUR COMPILER OF CHOICE
+*/
+
+
+using namespace std;
+
+void swap(char* point1,char* point2)
+{
+  //Store first pointer in temp var
+  char* temp = point1;
+  //assign first pointer to second
+  point1=point2;
+  //assign pont to to prev point 1
+  point2=temp;
+
+}
+
+string reverse(string const str)
+{
+  
+    int n = str.length();
+    string rev = str;
+ 
+    for (int i=0, j=n-1; i<j; i++,j--)
+    {
+      swap(rev[i], rev[j]);
+    }
+    
+    return rev;
+}
+ 
+char reVal(int num)
+{
+  //If between 0 and 9 take the number and add ascii for 0 else add ascii for A
+  return (num>=0&&num<=9)? ((char)(num + '0')) : (char)(num - 10 + 'A');
+}
+string baseConv(int base, int inputNum)
+{
+    // Store the result
+    string result = "";
+    // Repeatedly divide inputNum by base and take remainder
+    while (inputNum > 0) 
+    {
+        // Update result
+        result += reVal(inputNum % base);
+        // Update inputNum
+        inputNum /= base;
+    }
+    // Reverse the result
+    reverse(result);
+    return result;
+}
+
+
+
+
+int main()
+{
+  //36^5 is the max num of possible codes via Multiplication Rule
+  long int max = pow(36,5);
+  long int min =  1679615; //First 5-digit base 36 number
+  //File Output
+  ofstream Out;
+  Out.open("Attendance_Codes.txt");
+
+  //Generate Decimal numbers from 0 to 36^5
+ for(;min<max;min++)
+ {
+    //Convert them to Base-36 and throw them into a file with a tab
+    Out<<baseConv(36,min)<<"\t";
+    //add a newline
+    if(min%13==0)
+    {
+      Out<<"\n";
+    }
+  }
+  cout<<"Write Complete";
+  return 0;
+} 
